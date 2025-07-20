0202764d4c1912228c7345089253917a567819c2
Lucas Monzón <lucasm.ddr3@gmail.com>
2025-07-20 19:00:00 -0300

    removed unnecesary import

--- src/components/SnippetNav.vue
+++ src/components/SnippetNav.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { Plus } from 'lucide-vue-next';
-import { Label } from '@/components/ui/label';
 
 const emit = defineEmits<{
   (e: 'add-snippet', value: boolean): void;
@@ -13,10 +12,10 @@ const emitNewSnippet = () => {
 
 <template>
   <div class="h-14 w-full bg-primary flex flex-row items-center space-between justify-between p-4">
-    <img src="../assets/SnipplyLogo.png" alt="Snipply Logo" class="w-[8rem] h-auto" />
+    <img src="../assets/SnipplyLogo.png" alt="Snipply Logo" class="w-[7rem] h-auto" />
     <!-- Need to add v-if statement to the element below, so if we're creating a new snippet, it will not show the "Create Snippet" button. -->
     <div class="p-1 flex items-center gap-2 newSnippetWrapper clickable" @click="emitNewSnippet"> 
-      <Plus :size="25" absoluteStrokeWidth color="white" class="clickable"/>
+      <Plus :size="22" absoluteStrokeWidth color="white" class="clickable"/>
     </div>
   </div>
 </template>
