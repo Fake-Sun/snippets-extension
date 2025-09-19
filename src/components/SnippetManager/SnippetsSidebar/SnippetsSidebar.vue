6267ba5164a459d2c2509349d1c6597bb8417649
Lucas Monzón <lucasm.ddr3@gmail.com>
2025-09-19 00:57:45 -0300

    modified variables

--- src/components/SnippetManager/SnippetsSidebar/SnippetsSidebar.vue
+++ src/components/SnippetManager/SnippetsSidebar/SnippetsSidebar.vue
@@ -13,7 +13,7 @@ import {
 } from "@/components/ui/sidebar"
 
 // Menu items.
-const items = [
+const folders = [
   {
     title: "Fabletics",
     url: "#",
@@ -52,11 +52,11 @@ const items = [
         </SidebarGroupAction>
         <SidebarGroupContent>
           <SidebarMenu>
-              <SidebarMenuItem v-for="item in items" :key="item.title">
+              <SidebarMenuItem v-for="folder in folders" :key="folder.title">
                 <SidebarMenuButton asChild>
-                    <a :href="item.url">
-                      <component :is="item.icon" />
-                      <span>{{item.title}}</span>
+                    <a :href="folder.url">
+                      <component :is="folder.icon" />
+                      <span>{{folder.title}}</span>
                     </a>
                 </SidebarMenuButton>
               </SidebarMenuItem>
